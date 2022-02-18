d31d53983dedff1b9453ac0f8405f4322b728724
Daniel Barcelona <daniel.barcelona@urv.cat>
2022-02-18 15:59:14 +0100

    Test Stream

---
+++ src/main/java/org/example/TestStream.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import org.apache.crail.*;
+import org.apache.crail.active.BasicAction;
+import org.apache.crail.conf.CrailConfiguration;
+import org.apache.crail.core.ActiveWritableChannel;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class TestStream {
+  public static void main(String[] args) throws Exception {
+    // Load configuration from crail home config
+    CrailConfiguration conf = CrailConfiguration.createConfigurationFromFile();
+    CrailStore store = CrailStore.newInstance(conf);
+
+    String filename = "/test-object";
+
+    try {
+      store.delete(filename, true).get();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    // Create
+    CrailObject obj = store.create(filename, CrailNodeType.OBJECT, CrailStorageClass.DEFAULT,
+        CrailLocationClass.DEFAULT, false).get().asObject();
+    obj.syncDir();
+    CrailObjectProxy proxy = obj.getProxy();
+    System.out.println("Path: " + obj.getPath() + ", id: " + obj.getFd());
+    proxy.create(BasicAction.class);
+
+    // Stream write
+    Path path = Paths.get("/Datasets/wiki1/AA/wiki_00");
+    try (FileChannel channel = FileChannel.open(path, StandardOpenOption.READ)) {
+      ActiveWritableChannel writableChannel = proxy.getWritableChannel();
+      ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024); // 16 KB buffer
+
+      while (channel.read(buffer) != -1) {
+        System.out.println("buffer = " + buffer);
+        buffer.flip();
+        writableChannel.write(buffer);
+        buffer.clear();
+      }
+      writableChannel.close();
+    }
+    System.out.println("Files.lines(path).count() = " + Files.lines(path).count());
+    try (FileChannel channel = FileChannel.open(path, StandardOpenOption.READ)) {
+      InputStream stream = Channels.newInputStream(channel);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+//      System.out.println("file lines = " + reader.lines().count());
+      reader.lines().forEach(l -> System.out.println(l.substring(1,50)));
+    }
+
+    // Stream read
+    InputStream is = proxy.getInputStream();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(is, 16*1024)));
+//		System.out.println("file lines = " + reader.lines().count());
+    reader.lines().forEach(l -> System.out.println(l.substring(1, 50)));
+
+  }
+}
